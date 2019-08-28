0a5413525972ef191e54707212f25193d6791459
Vasilis Kassikos <vasilisK@esq2.com>
2019-08-28 20:51:25 +0300

    settings tab updated , font removed from orderGame

--- app/src/main/java/com/example/newdiplwm/SettingsClass.java
+++ app/src/main/java/com/example/newdiplwm/SettingsClass.java
@@ -2,11 +2,32 @@ package com.example.newdiplwm;
 
 import android.os.Bundle;
 
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreferenceCompat;
 
 public class SettingsClass extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.settings_prefs);
+
+        final SwitchPreferenceCompat switchPreferenceCompat = (SwitchPreferenceCompat) getPreferenceManager().findPreference("rememberME");
+        switchPreferenceCompat.setSummaryProvider(new Preference.SummaryProvider() {
+            @Override
+            public CharSequence provideSummary(Preference preference) {
+                if (switchPreferenceCompat.isChecked()) {
+                    return "Ενεργό";
+                }
+                return "Ανενεργό";
+
+
+            }
+        });
+       // switchPreferenceCompat.setIcon(R.drawable.arrowdown24);
+        //TODO refresh settings activity
+//        android:summaryOn="Ενεργό"
+//        android:summaryOff="Ανενεργό"
     }
 }
